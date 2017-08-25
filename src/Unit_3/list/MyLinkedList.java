c3e5c34943de11f2c95ae281e580c90d3ce9be1b
nirpis.hu <nirpis.hu@lianshang.com>
2017-08-25 10:45:51 +0800

    ArrayList和LinkedList的实现

---
+++ src/Unit_3/list/MyLinkedList.java
@@ -0,0 +1,150 @@
+package Unit_3.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by huwen on 2017/8/24.
+ */
+public class MyLinkedList<AnyType> implements Iterable<AnyType> {
+
+    private static class Node<AnyType> {
+        public AnyType data;
+        public Node<AnyType> prev;
+        public Node<AnyType> next;
+
+        public Node(AnyType data, Node<AnyType> prev, Node<AnyType> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private int theSize;
+
+    private int modCount = 0;
+
+    private Node<AnyType> beginMarker;
+
+    private Node<AnyType> endMarker;
+
+    public void clear() {
+        doClear();
+    }
+
+    private void doClear() {
+        beginMarker = new Node<>(null, null, null);
+        endMarker = new Node<>(null, beginMarker, null);
+        beginMarker.next = endMarker;
+
+        theSize = 0;
+        modCount++;
+    }
+
+    public int size() {
+        return theSize;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private void add(int idx, AnyType x) {
+        addBefore(getNode(idx, 0, size()), x);
+    }
+
+    public AnyType get(int idx) {
+        return getNode(idx).data;
+    }
+
+    public AnyType set(int idx, AnyType x) {
+        Node<AnyType> p = getNode(idx);
+        AnyType old = p.data;
+        p.data = x;
+        return old;
+    }
+
+    public AnyType remove(int idx) {
+        return remove(getNode(idx));
+    }
+
+    private void addBefore(Node<AnyType> p, AnyType x) {
+        Node<AnyType> newNode = new Node<>(x, p.prev, p);
+        newNode.prev.next = newNode;
+        p.prev = newNode;
+        theSize++;
+        modCount++;
+    }
+
+    private AnyType remove(Node<AnyType> p) {
+        p.next.prev = p.prev;
+        p.prev.next = p.next;
+        theSize--;
+        modCount++;
+        return p.data;
+    }
+
+    private Node<AnyType> getNode(int idx) {
+        return getNode(idx, 0, size() - 1);
+    }
+
+    private Node<AnyType> getNode(int idx, int lower, int upper) {
+        Node<AnyType> p;
+
+        if (idx < lower || idx > upper)
+            throw new IndexOutOfBoundsException();
+
+        if (idx < size() / 2) {
+            p = beginMarker.next;
+            for (int i = 0; i < idx; i++)
+                p = p.next;
+        } else {
+            p = endMarker;
+            for (int i = size(); i > idx; i--)
+                p = p.prev;
+        }
+        return p;
+    }
+
+    @Override
+    public Iterator<AnyType> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<AnyType> {
+
+        private Node<AnyType> current = beginMarker.next;
+        private int expectedModCurrent = modCount;
+        private boolean okToRemove = false;
+
+        @Override
+        public boolean hasNext() {
+            return current != endMarker;
+        }
+
+        @Override
+        public AnyType next() {
+            if (modCount != expectedModCurrent)
+                throw new ConcurrentModificationException();
+            if (!hasNext())
+                throw new NoSuchElementException();
+
+            AnyType nextItem = current.data;
+            current = current.next;
+            okToRemove = true;
+            return nextItem;
+        }
+
+        @Override
+        public void remove() {
+            if (modCount != expectedModCurrent)
+                throw new ConcurrentModificationException();
+            if (!okToRemove)
+                throw new IllegalStateException();
+            MyLinkedList.this.remove(current.prev);
+            expectedModCurrent++;
+            okToRemove = false;
+        }
+    }
+}
---
+++ src/Unit_3/stack/ArrayStack.java
@@ -0,0 +1,15 @@
+package Unit_3.stack;
+
+/**
+ * Created by huwen on 2017/8/24.
+ */
+public class ArrayStack<AnyType> {
+    private int theSize;
+
+    private AnyType [] theItem;
+
+    private final int DEFAULT_CAPACITY = 10;
+
+
+
+}
